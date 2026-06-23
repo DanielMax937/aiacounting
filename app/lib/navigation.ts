4829a06c232af1b0fb23bf7a512636b4c539c296
daniel <riyueniao2010@gmail.com>
2026-06-23 15:20:12 +0800

    Update next-intl navigation helper

--- app/lib/navigation.ts
+++ app/lib/navigation.ts
@@ -1,4 +1,8 @@
-import { createSharedPathnamesNavigation } from 'next-intl/navigation';
+import { createNavigation } from 'next-intl/navigation';
 import { locales } from '../../config';
 
-export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation({ locales }); 
+export const { Link, redirect, usePathname, useRouter } = createNavigation({
+  locales,
+  defaultLocale: 'zh',
+  localePrefix: 'always'
+}); 
